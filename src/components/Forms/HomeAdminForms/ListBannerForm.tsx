a5eee53307e8e1879ab9e706518dd943154eb8ba
aenewsss <aenamartinelli@gmail.com>
2024-02-23 19:39:07 -0300

    Banner update 100% funcional

--- src/components/Forms/HomeAdminForms/ListBannerForm.tsx
+++ src/components/Forms/HomeAdminForms/ListBannerForm.tsx
@@ -39,10 +39,14 @@ export default function ListBannerForm() {
         messageUpdate.updated && setEditing({ currentEditing: false, bannerId: null })
     }, [messageUpdate]);
 
-    if(banners.length == 0) return <p>Carregando</p>
+    function sameIdCurrentBanner(bannerId: string) {
+        return (currentBannerToEdit && currentBannerToEdit.id == bannerId)
+    }
+
+    if (banners.length == 0) return <p>Carregando</p>
 
     return banners.map((banner => (
-        <div className=" border-bottom border-black pb-5">
+        <div className=" border-bottom border-black pb-5 mt-3">
             <form action={updateBannerAction} key={banner.id} className="text-start">
                 <div className="d-flex flex-md-nowrap flex-wrap justify-content-between">
                     <input type="text" value={banner.id} className="d-none" name="id" />
@@ -50,8 +54,8 @@ export default function ListBannerForm() {
                         <label className="fw-medium fs-5">Imagem grande</label>
                         <span className="fs-14">Imagem grande que aparece em telas maiores<br />(computadores, tablets).<br />Tamanho recomendado: 1600px/600px.</span>
                         <div className="position-relative w-300 h-200" >
-                            <Image width={300} height={200} src={currentBannerToEdit ? currentBannerToEdit.large_image : banner.large_image} alt={banner.large_image} />
-                            <div onClick={() => changeEditing(banner.id!)} className="opacity-0 large-image-edit top-0 w-100 h-100 d-flex justify-content-center align-items-center position-absolute">
+                            <Image width={300} height={200} src={sameIdCurrentBanner(banner.id!) ? currentBannerToEdit!.large_image : banner.large_image} alt={sameIdCurrentBanner(banner.id!) ? currentBannerToEdit!.large_image : banner.large_image} />
+                            <div onClick={() => changeEditing(banner.id!)} className={`${!sameIdCurrentBanner(banner.id!) && 'pe-none'} opacity-0 large-image-edit top-0 w-100 h-100 d-flex justify-content-center align-items-center position-absolute`}>
                                 <label className="text-white z-1 fs-3 mb-0" htmlFor="large_image">Trocar imagem</label>
                                 <div className="position-absolute w-100 h-100 bg-black opacity-50"></div>
                             </div>
@@ -62,8 +66,8 @@ export default function ListBannerForm() {
                         <label className="fw-medium fs-5">Imagem pequena</label>
                         <span className="fs-14">Imagem pequena que aparece em telas menores<br /> (celular).<br /> Tamanho recomendado: 800px/600px.</span>
                         <div onClick={() => changeEditing(banner.id!)} className="position-relative w-300 h-200" >
-                            <Image width={300} height={200} src={banner.small_image} alt={banner.small_image} />
-                            <div className="small-image-edit opacity-0 small-image-edit top-0 w-100 h-100 d-flex justify-content-center align-items-center position-absolute">
+                            <Image width={300} height={200} src={sameIdCurrentBanner(banner.id!) ? currentBannerToEdit!.small_image : banner.small_image} alt={sameIdCurrentBanner(banner.id!) ? currentBannerToEdit!.small_image : banner.small_image} />
+                            <div className={`${!sameIdCurrentBanner(banner.id!) && 'pe-none'} opacity-0 small-image-edit top-0 w-100 h-100 d-flex justify-content-center align-items-center position-absolute`}>
                                 <label className="text-white z-1 fs-3 mb-0" htmlFor="small_image">Trocar imagem</label>
                                 <div className="position-absolute w-100 h-100 bg-black opacity-50"></div>
                             </div>
@@ -74,12 +78,12 @@ export default function ListBannerForm() {
                         <label className="fs-5 fw-medium" htmlFor={`formLink${banner.id}`}>Link</label>
                         <span className="fs-14">Link que deve abrir ao clicar no banner.</span>
                         <input
-                            disabled={!editing.currentEditing}
+                            disabled={!sameIdCurrentBanner(banner.id!)!}
                             type="text"
                             className="form-control w-100"
                             name={`link`}
                             id={`formLink${banner.id}`}
-                            value={!currentBannerToEdit ? banner.link : currentBannerToEdit?.link || ""}
+                            value={currentBannerToEdit && (currentBannerToEdit.id == banner.id) ? currentBannerToEdit?.link || "" : banner.link}
                             onChange={(e: any) => setCurrentBannerToEdit({ ...currentBannerToEdit!, link: e.target.value })}
                         />
                         <span onClick={() => changeEditing(banner.id!)} role="button" className="text-primary">Click aqui para editar o link</span>
